a27a2c0d55cc07a7c572094f3b5309662afcec23
Simon Mall <simonkmall@gmail.com>
2016-11-04 13:51:49 +0100

    Added Name Generator clas

---
+++ Tests/Model/NameGeneratorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Simon Mall.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+namespace Mallapp\GeobitsBundle\Tests\Model;
+
+use Mallapp\GeobitsBundle\Model\NameGenerator;
+
+/**
+ * Description of NameGeneratorTest
+ *
+ * @author Simon Mall
+ */
+class NameGeneratorTest extends \PHPUnit_Framework_TestCase {
+
+    public function testXY() {
+        
+        $name = NameGenerator::createName(47.385777, 8.500454);
+        
+        $this->assertEquals($name, "Baslerstrasse Zürich - Kreis 9 (CH)");
+
+    }
+
+}
